9e58535739e99fc15ecc7cb0c22239ffc6f45731
lucadanielcanas7 <lucadanielcanas7@gmail.com>
2022-06-21 17:42:26 -0300

    .

---
+++ src/app/gifs/result/result.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-result',
+  templateUrl: './result.component.html',
+  styles: [
+  ]
+})
+export class ResultComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
