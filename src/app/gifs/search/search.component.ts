1b252947c9bf6d32dbffa1209f632099032d58c9
lucadanielcanas7 <lucadanielcanas7@gmail.com>
2022-06-21 23:10:39 -0300

    txtSearch property created for search input element

--- src/app/gifs/search/search.component.ts
+++ src/app/gifs/search/search.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-search',
@@ -6,11 +6,15 @@ import { Component, OnInit } from '@angular/core';
   styles: [
   ]
 })
-export class SearchComponent implements OnInit {
+export class SearchComponent {
 
-  constructor() { }
+  //Select the view query element (input)
+  @ViewChild('txtSearch') txtSearch!: ElementRef<HTMLInputElement>;
 
-  ngOnInit(): void {
+  search(): void {
+    const value = this.txtSearch.nativeElement.value;
+    console.log(value);
+    this.txtSearch.nativeElement.value = '';
   }
 
 }
