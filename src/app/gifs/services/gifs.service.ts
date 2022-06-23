a2bd128dc575c09bc09ebebc4c5834e468ff41f0
lucadanielcanas7 <lucadanielcanas7@gmail.com>
2022-06-22 23:21:28 -0300

    call the api for get gifs

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,24 +1,39 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Gif, SearchGifsResponse } from '../interfaces/gifs.interface';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
+  private apiKey: string = 'B3elKYya4Lu25D4oV7DJaWmDQMOOmMoH';
   private _userHistory: string[] = [];
+
+  public results: Gif[] = [];
 
   get userHistory(): string[] {
     return [...this._userHistory];
   }
 
-  searchGifs(query: string) {
-    
+  constructor(private http: HttpClient) { }
+
+  //#region PUBLIC METHODS
+  searchGifs(query: string, pagination: number) {
     query = query.trim().toLowerCase();
 
     if (!this._userHistory.includes(query)) {
       this._userHistory.unshift(query);
       this._userHistory = this._userHistory.splice(0, 10);
     }
-    console.log(this._userHistory);
+
+    this.http.get<SearchGifsResponse>(
+      `https://api.giphy.com/v1/gifs/search?api_key=${this.apiKey}&q=${query}&limit=${pagination}`
+      )
+      .subscribe( (res) => {
+        this.results = res.data;
+      });
   }
+  //#endregion
+
 }
